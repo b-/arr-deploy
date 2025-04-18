d0e93245a232ae157520d4ed63e7fee5aab86d0d
bri <284789+b-@users.noreply.github.com>
2025-04-17 21:51:48 -0400

    .

---
+++ compose_this.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+# This script wraps docker compose with the required arguments for this project.
+
+# The root of this compose project, fetched via git
+compose_root="$(git rev-parse --show-toplevel)"
+compose_flags=(
+  --project-name "${APP_NAME}"
+  --file "${compose_root}/compose.yaml"
+  --file "${compose_root}/compose.override.yaml"
+  --env-file "${compose_root}/compose.env"
+)
+cd "${compose_root}"
+exec docker compose "${compose_flags[@]}" "$@"
